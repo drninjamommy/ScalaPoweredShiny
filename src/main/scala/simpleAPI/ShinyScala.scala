d0e7abb2c163655fd3827276d49f1d4e70ebe672
dan.dixey <dan.dixey@gmail.com>
2017-06-05 22:30:50 +0100

    Removing types and adding a build properties

--- src/main/scala/simpleAPI/ShinyScala.scala
+++ src/main/scala/simpleAPI/ShinyScala.scala
@@ -18,7 +18,7 @@ import scalaz.concurrent.Task
 object ShinyScala extends ServerApp {
 
   val port: Int = envOrNone("HTTP_PORT") map (_.toInt) getOrElse 8080
-  val ip: String = "0.0.0.0"
+  val ip = "0.0.0.0"
   val pool: ExecutorService = Executors.newCachedThreadPool()
 
   override def server(args: List[String]): Task[Server] =
@@ -33,20 +33,18 @@ object ShinyScala extends ServerApp {
   lazy val pressureData: Json = getJson("pressure.csv")
 
   val service = HttpService {
-    case GET -> Root / "data" / name => {
+    case GET -> Root / "data" / name =>
       name match {
         case "rocks" => Ok(Json.obj("payload" -> rockData))
         case "cars" => Ok(Json.obj("payload" -> carData))
         case "pressure" => Ok(Json.obj("payload" -> pressureData))
         case _ => NoContent()
       }
-    }
-    case req @ POST -> Root / "message" => {
+    case req @ POST -> Root / "message" =>
       for {
         user <- req.as(jsonOf[data])
         resp <- Ok(Json.obj("Message" -> Json.fromString(
           s"Hello ${Hello(user.data.name).greeting} this is your http4s API")))
       } yield resp
-    }
   }
 }
